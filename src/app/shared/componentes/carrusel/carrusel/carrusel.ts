0da54de2b44ea8a88784568da5ed09024938a969
PhixAlek <miguelsegura618@gmail.com>
2025-10-13 21:49:38 -0500

    fix(config): corrige rutas de assets y carga de Bootstrap
    - Se agregó "src/assets" al bloque de build para copiar las imágenes correctamente.
    - Se enlazaron los archivos CSS y JS de Bootstrap en los estilos globales.
    - Soluciona el problema del carrusel que no mostraba imágenes.

--- src/app/shared/componentes/carrusel/carrusel/carrusel.ts
+++ src/app/shared/componentes/carrusel/carrusel/carrusel.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+/*
+  Importaciones principales
+  -------------------------
+  Angular trabaja de manera modular:
+  importamos solo lo que necesitamos para este componente.
+*/
+import { Component } from '@angular/core';    // Permite declarar una clase como componente de Angular.
+import { CommonModule } from '@angular/common'; // Proporciona directivas comunes (como @for, @if, ngClass, etc.).
 
+
+/*
+  Decorador @Component
+  --------------------
+  Aquí definimos la configuración del componente:
+  cómo se llama, dónde está su HTML, sus estilos y qué módulos necesita.
+*/
 @Component({
-  selector: 'app-carrusel',
-  imports: [],
-  standalone:true,
-  templateUrl: './carrusel.html',
-  styleUrl: './carrusel.scss'
+  selector: 'app-carrusel',            // Nombre con el que podremos usar el componente en el HTML (<app-carrusel></app-carrusel>)
+  standalone: true,                    // Indica que este componente NO depende de un módulo (Angular moderno: standalone)
+  imports: [CommonModule],             // Importa las herramientas básicas de Angular (necesarias para @for, [class], etc.)
+  templateUrl: './carrusel.html',      // Ruta del archivo HTML asociado (vista del componente)
+  styleUrls: ['./carrusel.scss']       // Ruta del archivo SCSS asociado (estilos del componente)
 })
+
+
+/*
+  Clase del componente
+  --------------------
+  Representa la "lógica" del carrusel.
+  Aquí van las propiedades (datos) y métodos (funciones) que usa la vista (HTML).
+*/
 export class Carrusel {
 
+  /*
+    Propiedad 'imagenes'
+    --------------------
+    Es un arreglo (array) que contiene las rutas de las imágenes que
+    se mostrarán en el carrusel.
+    El HTML recorrerá este arreglo con la directiva @for.
+  */
+  imagenes = [
+    'assets/images/1.webp',
+    'assets/images/amd-4k.jpg',
+    'assets/images/onepiece.jpg',
+    'assets/images/shaman-king.jpg',
+    'assets/images/prueba_1.png'
+  ];
 }
