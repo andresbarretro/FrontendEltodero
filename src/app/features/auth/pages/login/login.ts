8f3067aea8221f9ab330a5ddc8528c75284624bb
andresbarretro <andres_bandres@outlook.com>
2025-10-14 17:01:32 -0500

    cambios en componentes

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,44 +1,52 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from "../../services/auth"; // ajusta el path según tu estructura
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [
-    CommonModule, FormsModule],
   standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrls: ['./login.scss']
 })
 export class Login {
   email = '';
   password = '';
+  isLoggedIn = false;
 
-constructor(
+  @Output() cerrarModal = new EventEmitter<void>();
+
+  constructor(
     private router: Router,
     private authService: AuthService
   ) {}
 
-  onSubmit(){
-    if(!this.email || !this.password){
+  onSubmit() {
+    if (!this.email || !this.password) {
       alert("Por favor complete ambos campos");
-      return
-  }
+      return;
+    }
+
     // Simulación de login (sin API)
     const ok = this.authService.loginMock(this.email, this.password);
     if (ok) {
       this.router.navigate(['/home']);
+      this.isLoggedIn = true;
+      this.cerrarModal.emit(); // cerrar modal al iniciar sesión
     } else {
       alert('Credenciales inválidas (simulación).');
     }
   }
 
-  @Output() cerrarModal = new EventEmitter<void>();
+  logout() {
+    this.authService.logout();
+    this.isLoggedIn = false;
+  }
 
   cerrar(event: MouseEvent) {
-    // Se ejecuta al hacer clic en el overlay
+    // Si el usuario hace clic fuera del modal
     if (event.target === event.currentTarget) {
       this.cerrarModal.emit();
     }
@@ -47,8 +55,5 @@ constructor(
   iniciarSesion() {
     console.log('Iniciando sesión...');
     this.cerrarModal.emit();
-  }
+  }
 }
-
-
-
--- src/app/shared/componentes/header/header.ts
+++ src/app/shared/componentes/header/header.ts
@@ -1,12 +1,22 @@
+import { Login } from './../../../features/auth/pages/login/login';
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, Login],
   standalone: true,
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class header {
+  mostrarLogin = false;
+
+  abrirLogin() {
+    this.mostrarLogin = true;
+  }
+
+  cerrarLogin() {
+    this.mostrarLogin = false;
+  }
 
 }
