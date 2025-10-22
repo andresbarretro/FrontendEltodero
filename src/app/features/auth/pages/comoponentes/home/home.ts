2b39fc5fc8e5ebbc9f3cd2569317ae74a7395d61
andresbarretro <andres_bandres@outlook.com>
2025-10-22 18:54:55 -0500

    cards @for

--- src/app/features/auth/pages/comoponentes/home/home.ts
+++ src/app/features/auth/pages/comoponentes/home/home.ts
@@ -2,10 +2,18 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Header } from '../../../../../shared/componentes/header/header';
 import { Main } from '../../../../../shared/componentes/main/main';
 import { Carrusel } from '../../../../../shared/componentes/carrusel/carrusel/carrusel';
+import { CommonModule } from '@angular/common';
 
+interface producto {
+  imagen: string;
+  id: number;
+  name: string;
+  price: number;
+  description:string;
+}
 @Component({
   selector: 'app-home',
-  imports: [Header, Main, Carrusel],
+  imports: [Header, Main, Carrusel, CommonModule],
   standalone:true,
   templateUrl: './home.html',
   styleUrl: './home.scss'
@@ -19,5 +27,28 @@ export class Home {
 
   scrollRight() {
     this.carousel.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+
+  loading: true;
+  productos: producto[] = [];
+
+  ngOnInit() {
+    // Simulamos carga de productos
+    setTimeout(() => {
+      this.productos = [
+        { id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
+        { id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
+        { id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
+        { id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
+        { id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
+        { id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
+        { id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
+        { id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
+        { id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
+        { id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
+        { id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
+        { id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
+      ];
+      this.loading = false;
+    }, 1500);
   }
-}
+} }
