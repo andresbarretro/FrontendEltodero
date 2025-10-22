0535010ecd072db9ecb451294d57fe4ae8638170
andresbarretro <andres_bandres@outlook.com>
2025-10-21 20:34:53 -0500

    cambios main.html @for

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/auth/pages/comoponentes/home/home';
 import { Nosotros } from './features/auth/pages/comoponentes/nosotros/nosotros';
+import { Header } from './shared/componentes/header/header';
+import { Login } from './features/auth/pages/login/login';
 
 export const routes: Routes =[
+
   { path: 'home', component: Home },
   { path: 'nosotros', component: Nosotros},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'header', component: Header },
+  { path: 'Login', component: Login },
+
+  { path: '**', redirectTo: 'header', pathMatch: 'full' }
 ];
--- src/app/shared/componentes/main/main.ts
+++ src/app/shared/componentes/main/main.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 interface Product {
+  imagen: string;
   id: number;
   name: string;
   price: number;
@@ -22,18 +23,23 @@ export class Main {
     // Simulamos carga de productos
     setTimeout(() => {
       this.products = [
-        { id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
-        { id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
-        { id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
-        { id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
-        { id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
-        { id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
-        { id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
-        { id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
-        { id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
-        { id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
-        { id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
-        { id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
+       { imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
+       { imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
+       { imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg",id: 1, name: 'Monitor 4K', price: 250, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 2, name: 'Teclado mecánico', price: 120, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://www.wepc.com/wp-content/uploads/2019/05/DSC02079-1-scaled.jpg", id: 3, name: 'Mouse ergonómico', price: 80, description: "esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://i.ebayimg.com/images/g/kVAAAOSw~bFm0cXI/s-l1600.webp",id:5, name:"gorra negra", price:15, description:"esto es una prieda corta sobre la descripcion del producto" },
+        {imagen:"https://i.ebayimg.com/images/g/kVAAAOSw~bFm0cXI/s-l1600.webp",id:5, name:"gorra negra", price:15, description:"esto es una prieda corta sobre la descripcion del producto" }
       ];
       this.loading = false;
     }, 1500);
